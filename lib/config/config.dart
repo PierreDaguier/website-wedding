bff6a2764a561f6d9fe78126826170fdd25e5037
Pierre Daguier <63846340+PierreDaguier@users.noreply.github.com>
2026-01-13 14:02:36 +1000

    Add secrets file support for contact info

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,6 +3,12 @@ class ContactConfig {
       String.fromEnvironment('PHONE_PIERRE', defaultValue: '');
   static const String phoneJoanne =
       String.fromEnvironment('PHONE_JOANNE', defaultValue: '');
+  static const String phoneContact =
+      String.fromEnvironment('PHONE_CONTACT', defaultValue: '');
   static const String emailContact =
       String.fromEnvironment('EMAIL_CONTACT', defaultValue: '');
+  static const String emailJoanne =
+      String.fromEnvironment('EMAIL_JOANNE', defaultValue: '');
+  static const String emailPierre =
+      String.fromEnvironment('EMAIL_PIERRE', defaultValue: '');
 }
